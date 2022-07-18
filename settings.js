11b115c403eed153a1b34ea944129e5dabbdd28e
Sabta Mendes <sabtaamendes@gmail.com>
2022-07-18 11:48:43 -0400

    modal

--- settings.js
+++ settings.js
@@ -17,29 +17,38 @@
 }*/
 
 
+/*let food = null;
+let nameF = null;
+let drink = null;
+let dessert = null;
+let total = 0;*/
 
 function foodSelection(element, foodsName, value) {
     console.log(element, foodsName, value)
     let card = document.querySelector(".borderGreen");
-    let icon = document.querySelector(".icon-check");
+    let icon = document.querySelector(".hidden");
     if (card !== null) {
         card.classList.remove('borderGreen'); //nao precisa está no DOM, A Classe da borda
         icon.classList.add('hidden');
     }
-    element.classList.add('borderGreen'); //nao precisa está no DOM, A Classe da borda
-    element.classList.remove('hidden');
-    greenLight();
-}
 
+    element.classList.add('borderGreen'); //nao precisa está no DOM, A Classe da borda
+   /* nameF = foodsName;
+    total = value;
+    food = element.innerHTML;
+    greenLight();*/
+}
+/*
 function drinkSelection(element) {
     let card = document.querySelector(".drinkCards .borderGreen"); //para não desmarcar os cards do foodSelectio é necessário fazer uma descrição exata do elemento que quero manipular (esse eu peguei o conteiner que tem descendentes com a borda)
-    let icon = document.querySelector(".drinkCards .borderGreen .icon-check");
+    let icon = document.querySelector(".drinkCards .borderGreen .icon-check.hidden");
     if (card !== null) {
         card.classList.remove('borderGreen'); //nao precisa está no DOM, A Classe da borda
         icon.classList.add('hidden');
     }
     element.classList.add('borderGreen'); //nao precisa está no DOM, A Classe da borda
     element.classList.remove('hidden');
+    drink = element.innerHTML;
     greenLight();
 }
 
@@ -53,20 +62,27 @@ function dessertSelection(element) {
     }
     element.classList.add('borderGreen'); //nao precisa está no DOM, A Classe da borda
     element.classList.remove('hidden');
+    dessert = element.innerHTML;
     greenLight();
 }
 
 
-function greenLight() {
-    console.log(greenLight)
-    let green = document.querySelector('.border')
-    if (foodSelection !== null && drinkSelection !== null && dessertSelection !== null) {
+function greenLight(element) {
+    let green = document.querySelector('.changeColor')
+    if (foodSelection !== undefined && drinkSelection !== undefined && dessertSelection !== undefined) {
         green.classList.remove('changeColor')
-        green.innerHTML = '<p>Finalizar Pedido</p>'
+
     }
-   green.classList.add('changeColor')
+    element.classList.add('changeColor');
+    element.innerHTML = '<p>Finalizar Pedido</p>';
 }
 
-function fazerPedido(){
+function fazerPedido() {
     console.log(fazerPedido)
 }
+
+/*function modal(){
+
+}*/
+
+//quando eu clicar no último card o modal deve aparececr
