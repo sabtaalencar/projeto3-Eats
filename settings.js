5490db66a99d632ec20fa8507086c991b6270c41
Sabta Mendes <sabtaamendes@gmail.com>
2022-07-19 10:22:20 -0400

    projeto em andamento

--- settings.js
+++ settings.js
@@ -1,8 +1,34 @@
-function foodSelection(element){
-    let cards = document.querySelector(".borderGreen");
+function foodSelection(element) {
+    let cardsFood = document.querySelector(".borderGreen");
 
-    if(cards !== null){
-        cards.classList.remove("borderGreen")
+
+    if (cardsFood !== null) {
+        cardsFood.classList.remove("borderGreen");
+
     }
-    element.classList.add("borderGreen")
+    element.classList.add("borderGreen");
+
+}
+
+function drinkSelection(element) {
+    let card = document.querySelector(".drinkCards .borderGreen"); //para não desmarcar os cards do foodSelectio é necessário fazer uma descrição exata do elemento que quero manipular (esse eu peguei o conteiner que tem descendentes com a borda)
+
+    if (card !== null) {
+        card.classList.remove('borderGreen'); //nao precisa está no DOM, A Classe da borda
+    }
+    element.classList.add('borderGreen'); //nao precisa está no DOM, A Classe da borda
+
+
+}
+
+function dessertSelection(element) {
+    let cardsFood = document.querySelector(".dessertCards .borderGreen");
+
+
+    if (cardsFood !== null) {
+        cardsFood.classList.remove("borderGreen");
+
+    }
+    element.classList.add("borderGreen");
+
 }
